8162130a6be822d20ec3fffb7ecbb1ff0ab4378a
CAAT Data <data@caat.org.uk>
2022-02-08 18:41:47 +0100

    Write totals to README.

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 Data about arms and security trade fairs and the companies that exhibit at them.
 
 A collaborative project between [Campaign Against Arms Trade](https://caat.org.uk) and [Omega Research Foundation](https://omegaresearchfoundation.org/).
+
+## Statistics
+
+-   **30** series
+-   **171** events
+-   **49339** exhibitors
+-   **181** organisers
+-   **1231** delegations
+
+
+## References
--- python/summary.py
+++ python/summary.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import io
 import csv
 import json
+import shutil
 import logging
 import argparse
 from pathlib import Path
@@ -63,6 +65,14 @@ def main():
         "Website",
     )
 
+    count = {
+        "series": 0,
+        "events": 0,
+        "exhibitors": 0,
+        "organisers": 0,
+        "delegations": 0,
+    }
+
     out = [titles]
     last_series = None
     for path in args.json_path:
@@ -71,12 +81,15 @@ def main():
         organiser = data.get("organiser", "")
         if organiser:
             organiser = 1 if isinstance(organiser, dict) else len(organiser)
+            count["organisers"] += organiser
         exhibitor = data.get("exhibitor", "")
         if exhibitor:
             exhibitor = 1 if isinstance(exhibitor, dict) else len(exhibitor)
+            count["exhibitors"] += exhibitor
         delegation = data.get("delegation", "")
         if delegation:
             delegation = 1 if isinstance(delegation, dict) else len(delegation)
+            count["delegations"] += delegation
         website = data.get("website", "")
         if website and not isinstance(website, str):
             website = " ".join(website)
@@ -85,6 +98,7 @@ def main():
         if series != last_series:
             out.append(["", ] * len(titles))
             last_series = series
+            count["series"] += 1
 
         out.append((
             path.stem,
@@ -95,9 +109,34 @@ def main():
             str(delegation),
             website,
         ))
+        count["events"] += 1
 
     writer = csv.writer(args.csv_path.open("w"))
     writer.writerows(out)
+
+    readme_path = Path("README.md")
+    readme = readme_path.read_text("utf-8")
+    readme_out = io.StringIO()
+    section = 0
+    for line in readme.splitlines():
+        if "## Statistics" in line:
+            section = 1
+            readme_out.write(line + "\n")
+            readme_out.write("\n")
+            for k, v in count.items():
+                readme_out.write(f"-   **{v}** {k}\n")
+            readme_out.write("\n")
+            readme_out.write("\n")
+        elif "##" in line:
+            section = 2
+
+        if section in (0, 2):
+            readme_out.write(line + "\n")
+
+
+    with readme_path.open("w") as fp:
+        readme_out.seek(0)
+        shutil.copyfileobj(readme_out, fp)
 
 
 
